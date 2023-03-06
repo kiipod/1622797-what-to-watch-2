a3ad9e6a566738e9fcea08afe010bd128deba57b
kiipod <iamkir90@hotmail.com>
2023-03-07 01:35:30 +0300

    Добавляет контроллеры для API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    public function update()
+    {
+        //
+    }
+}

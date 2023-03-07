3874232b562eea8b7e3ff3c6192962444edb1ef2
kiipod <iamkir90@hotmail.com>
2023-03-07 19:32:48 +0300

    Переименовывает контроллеры

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FilmsController extends Controller
+class FilmController extends Controller
 {
     /**
      * Display a listing of the resource.

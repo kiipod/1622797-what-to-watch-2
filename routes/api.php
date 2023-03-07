5fd16386b2d1fa5f9467b35d1ead2e3cb402c0d5
kiipod <iamkir90@hotmail.com>
2023-03-07 19:32:04 +0300

    Добавляет названия маршрутов

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FavoriteController;
-use App\Http\Controllers\FilmsController;
-use App\Http\Controllers\GenresController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\PromoController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
@@ -21,47 +21,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'register'])->name('register.index');
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum')->name('auth.logout');
 
 Route::prefix('user')->middleware('auth:sanctum')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::patch('/{id}', [UserController::class, 'update']);
+    Route::get('/', [UserController::class, 'index'])->name('user.index');
+    Route::patch('/{id}', [UserController::class, 'update'])->name('user.update');
 });
 
 Route::group(['prefix' => 'films'], function () {
-    Route::get('/', [FilmsController::class, 'index'])->name('films.index');
-    Route::get('/{id}', [FilmsController::class, 'show'])->name('films.show');
-    Route::get('{id}/similar', [FilmsController::class, 'getSimilar'])->where('id', '\d+');
+    Route::get('/', [FilmController::class, 'index'])->name('films.index');
+    Route::get('/{id}', [FilmController::class, 'show'])->name('films.show');
+    Route::get('{id}/similar', [FilmController::class, 'getSimilar'])
+        ->where('id', '\d+')->name('films.similar');
     Route::get('{id}/comments', [CommentController::class, 'index'])
         ->where('id', '\d+')->name('comments.index');
 });
 
 Route::prefix('films')->middleware('auth:sanctum')->group(function () {
-    Route::post('/', [FilmsController::class, 'store']);
-    Route::patch('/{id}', [FilmsController::class, 'update'])->where('id', '\d+');
-    Route::post('/{id}/favorite', [FavoriteController::class, 'store'])->where('id', '\d+');
-    Route::delete('/{id}/favorite', [FavoriteController::class, 'destroy'])->where('id', '\d+');
+    Route::post('/', [FilmController::class, 'store'])->name('films.store');
+    Route::patch('/{id}', [FilmController::class, 'update'])
+        ->where('id', '\d+')->name('films.update');
+    Route::post('/{id}/favorite', [FavoriteController::class, 'store'])
+        ->where('id', '\d+')->name('favorite.store');
+    Route::delete('/{id}/favorite', [FavoriteController::class, 'destroy'])
+        ->where('id', '\d+')->name('favorite.destroy');
     Route::post('/{id}/comments', [CommentController::class, 'store'])
        ->where('id', 'd+')->name('comment.store');
 });
 
-Route::get('/genres', [GenresController::class, 'index'])->name('genres.index');
+Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
 
 Route::prefix('genres')->middleware('auth:sanctum')->group(function () {
-    Route::patch('/{genre}', [GenresController::class, 'update'])->name('genres.update');
+    Route::patch('/{genre}', [GenreController::class, 'update'])->name('genres.update');
 });
 
-Route::get('/favorite', [FavoriteController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/favorite', [FavoriteController::class, 'index'])
+    ->middleware('auth:sanctum')->name('favorite.index');
 
 Route::prefix('comments')->middleware('auth:sanctum')->group(function () {
-    Route::patch('/{comment}', [CommentController::class, 'update']);
+    Route::patch('/{comment}', [CommentController::class, 'update'])->name('comments.update');
     Route::delete('/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
 
 Route::prefix('promo')->middleware('auth:sanctum')->group(function () {
-    Route::get('/', [PromoController::class, 'index']);
-    Route::post('/{id}', [PromoController::class, 'store'])->where('id', '\d+');
+    Route::get('/', [PromoController::class, 'index'])->name('promo.index');
+    Route::post('/{id}', [PromoController::class, 'store'])
+        ->where('id', '\d+')->name('promo.store');
 });

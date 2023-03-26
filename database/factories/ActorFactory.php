6864b1cd414b1cefa2a834fa1f82ba456780264d
kiipod <iamkir90@hotmail.com>
2023-03-26 17:51:55 +0300

    Делает фабрики для моделей

---
+++ database/factories/ActorFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Actor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Actor>
+ */
+class ActorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name()
+        ];
+    }
+}
---
+++ database/factories/FilmFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Film;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Film>
+ */
+class FilmFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'poster_image' => $this->faker->imageUrl(),
+            'preview_image' => $this->faker->imageUrl(),
+            'background_image' => $this->faker->imageUrl(),
+            'background_color' => $this->faker->hexColor(),
+            'released' => $this->faker->dateTime(),
+            'description' => $this->faker->text(),
+            'run_time' => $this->faker->numberBetween(30, 200),
+            'video_link' => $this->faker->url(),
+            'preview_video_link' => $this->faker->url(),
+            'imdb_id' => 'tt' . $this->faker->unique()->randomNumber(7, true),
+            'status' => $this->faker->randomElement(['pending', 'moderate', 'ready']),
+            'rating' => $this->faker->randomFloat(2, 1, 10),
+        ];
+    }
+}

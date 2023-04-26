d3be84d94ac0da47a3de7938ec73e884272e36b5
kiipod <iamkir90@hotmail.com>
2023-04-26 23:48:23 +0300

    Создает тест для Комментариев

---
+++ tests/Feature/CommentRouteTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Comment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CommentRouteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_comment_get_route()
+    {
+        Comment::factory()->count(10)->create();
+
+        $this->getJson(route('comments.index'))
+            ->assertOk()
+            ->assertJsonStructure([
+                'data' => [
+                    'genres' => [['id', 'title']]
+                ]
+            ]);
+    }
+}
